94c7b61bef00635e6afb4db830a939591eaa01ef
Brij1707 <chug.brijesh@gmail.com>
2026-08-16 13:00:51 +0530

    second commit

--- Frontend/src/components/Layout/Topbar.jsx
+++ Frontend/src/components/Layout/Topbar.jsx
@@ -7,23 +7,32 @@ import { RiTwitterXLine } from "react-icons/ri";
 const Topbar = () => {
   return (
     <div className="bg-[#ea2e0e] text-white">
-      <div className="container mx-auto">
-        <div className="flex items-center space-x-4">
+      <div className="container mx-auto flex justify-between items-center py-3 px-4">
+        <div className="hidden md:flex items-center space-x-4">
           <div>
-            <Link to={"#"} className="hover:text-gray-300">
+            <a href={"#"} className="hover:text-gray-300">
               <TbBrandMeta className="h-5 w-5" />
-            </Link>
+            </a>
           </div>
           <div>
-            <Link to={"#"} className="hover:text-gray-300">
+            <a href={"#"} className="hover:text-gray-300">
               <IoLogoInstagram className="h-5 w-5" />
-            </Link>
+            </a>
           </div>
           <div>
-            <Link to={"#"} className="hover:text-gray-300">
+            <a href={"#"} className="hover:text-gray-300">
               <RiTwitterXLine className="h-4 w-4" />
-            </Link>
+            </a>
           </div>
+        </div>
+        <div className="text-sm text-center flex-grow">
+          <span>We shipped World-wide, Faster and Reliable Shipping!</span>
+        </div>
+
+        <div className="text-sm hidden md:block">
+          <a href="+01234567890" className="hover:text-gray-300">
+            +01 (234) 567890
+          </a>
         </div>
       </div>
     </div>
