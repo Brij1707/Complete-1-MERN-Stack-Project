af03c5ba98013bb60d57f2e5728edecd7c57c310
Brij1707 <chug.brijesh@gmail.com>
2026-08-18 09:04:53 +0530

    Fourth

--- Frontend/src/components/Common/Navbar.jsx
+++ Frontend/src/components/Common/Navbar.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import {HiOutlineUser,HiOutlineShoppingCart,HiBars3BottomRight} from 'react-icons/hi2'
 import Searchbar from './Searchbar'
+import CartDrawer from '../Layout/CartDrawer'
 
 
 
 const Navbar = () => {
+  const [drawerOpen,setDrawerOpen] = useState(false);
+
+
+    const toggleCartDrawer =()=>{
+        setDrawerOpen(!drawerOpen);
+    }
   return (
     <>
     <nav className='container mx-auto flex items-center justify-between py-6 px-6'>
@@ -33,7 +40,7 @@ const Navbar = () => {
          <Link to={'/profile'} className='hover:text-black'>
          <HiOutlineUser className='h-6 w-6 text-gray-700'/>
          </Link>
-         <button className=' hover:text-black relative'>
+         <button onClick={toggleCartDrawer} className=' hover:text-black relative'>
          <HiOutlineShoppingCart className='h-6 w-6 text-gray-700'/>
          <span className='absolute -top-2 bg-[#ea2e0e] text-white text-xs rounded-full px-2 py-.5'>4</span>
          </button>
@@ -44,6 +51,8 @@ const Navbar = () => {
          </button>
       </div>
     </nav>
+    <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer}/>
+    
     </>
   )
 }
--- Frontend/src/components/Common/Searchbar.jsx
+++ Frontend/src/components/Common/Searchbar.jsx
@@ -1,25 +1,35 @@
 import React, { useState } from 'react'
-import { HiMagnifyingGlass } from 'react-icons/hi2';
+import { HiMagnifyingGlass, HiMiniXMark } from 'react-icons/hi2';
 
 const Searchbar = () => {
-    const [searchTerm,setSearchTerm] = useState();
+    const [searchTerm,setSearchTerm] = useState("");
     const [isOpen,setIsOpen] = useState(false);
 
     const handleSerchToggle=()=>{
         setIsOpen(!isOpen);
+        }
+
+    const handleSearch = (e) =>{
+        e.preventDefault();
+        console.log("Search Term", searchTerm);
+        setIsOpen(false); 
+        
     }
 
   return (
     
       <div className={`flex items-center justify-center w-full transition-All duration-300 ${isOpen?"absolute top-0 left-0 w-full bg-white h-24 z-50":""}`}>
       
-      {isOpen?(<form className='relative flex items-center justify-center w-full'>
+      {isOpen?(<form onSubmit={handleSearch} className='relative flex items-center justify-center w-full'>
         <div className='relative w-1/2 flex items-center'>
-        <input type='text' value={searchTerm} placeholder='Search' className='bg-gray-100 px-4 py-2 pl-2 pr-12 rounded-lg focus:outline-none w-full placeholder:text-gray-700'></input>
+        <input type='text' onChange={(e)=> setSearchTerm(e.target.value)} value={searchTerm} placeholder='Search' className='bg-gray-100 px-4 py-2 pl-2 pr-12 rounded-lg focus:outline-none w-full placeholder:text-gray-700'></input>
         <button type='submit' className='absolute right-2'>
             <HiMagnifyingGlass className='h-6 w-6'/>
         </button>
         </div> 
+        <button onClick={handleSerchToggle} type='button' className='absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-600 hover:text-gray-800'>
+            <HiMiniXMark className='h-6 w-6' />
+        </button>
 
       </form>):(
         <button onClick={handleSerchToggle}>
