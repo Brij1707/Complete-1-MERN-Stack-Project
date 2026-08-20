8fcb174a9203aeb0480236b3dda514018a559c5c
Brij1707 <chug.brijesh@gmail.com>
2026-08-20 18:16:21 +0530

    fifth

--- Frontend/src/components/Layout/Hero.jsx
+++ Frontend/src/components/Layout/Hero.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import heroImg from '../../assets/rabbit-hero.webp'
 
 const Hero = () => {
   return (
-    <section>
-      <img src={heroImg} alt="rabbit" className='w-full h-[400px] md:h-[600px] lg:[750px] object-cover' />
+    <section className='relative '>
+      <img src={heroImg} alt="rabbit" className='w-full h-[400px] md:h-[600px] lg:h-[750px] object-cover' />
+      <div className='absolute inset-0 z-20 flex items-center justify-center'>
+        <div className='text-center text-white p-6'>
+        <h1 className='text-4xl md:text-9xl font-bold tracking-tighter uppercase mb-4'>
+          Vacation <br />Ready
+        </h1>
+        <p className='text-sm tracking-tighter md:text-lg mb-6'>
+          Explore our vacation-ready outfits with fast worldwide shipping.
+        </p>
+        <Link to="#" className='bg-white text-gray-950 rounded-sm px-6 py-2 text-lg' >Shop Now</Link>
+        </div>
+      </div>
     </section>
   )
 }
